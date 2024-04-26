ce8446ae4f952fb816e902d79b50643249474cb3
Gavin Popken <gavinpopkenart@gmail.com>
2024-04-26 11:38:47 -0500

    would be up to the point where i can train the model and get its accuracy, if tf.estimator wasnt depricated

--- practice.py
+++ practice.py
@@ -17,5 +17,51 @@ y_train = dftrain.pop('survived')
 y_eval = dfeval.pop('survived')
 # print(dftrain.describe())
 
-dftrain.age.hist(bins=20)
-plt.show()
+# **Shows the histogram of ages (like a bar graph)
+# not sure what the bins=20 represents
+# dftrain.age.hist(bins=20) 
+
+# **Plots the number of men and women, similar with the class (class must be a protected word in Python)
+# dftrain.sex.value_counts().plot(kind='barh')
+# dftrain['class'].value_counts().plot(kind='barh')
+
+# **Shows the percent of men and women that survived.
+# pd.concat([dftrain, y_train], axis=1).groupby('sex').survived.mean().plot(kind='barh').set_xlabel('% survive')
+# plt.show()
+
+# print(dfeval.shape)
+
+CATEGORICAL_COLUMNS = ['sex', 'n_siblings_spouses', 'parch', 'class', 'deck', 'embark_town', 'alone']
+
+NUMERIC_COLUMNS = ['age', 'fare']
+
+feature_columns = []
+for feature_name in CATEGORICAL_COLUMNS:
+    vocabulary = dftrain[feature_name].unique()  # gets a list of all unique values from given feature column
+    feature_columns.append(tf.feature_column.categorical_column_with_vocabulary_list(feature_name, vocabulary))
+
+for feature_name in NUMERIC_COLUMNS:
+    feature_columns.append(tf.feature_column.numeric_column(feature_name, dtype=tf.float32))
+
+# print(feature_columns)
+
+def make_input_fn(data_df, label_df, num_epochs=10, shuffle=True, batch_size=32):
+    def input_function(): # inner function, this will be returned
+        ds = tf.data.Dataset.from_tensor_slices((dict(data_df), label_df)) # creates tf.data.Dataset object with data and its labels
+        if shuffle:
+            ds = ds.shuffle(1000) # randomize order of data
+        ds = ds.batch(batch_size).repeat(num_epochs) # split dataset into batches of 32 and repeat process for number of epochs
+        return ds # returns a batch of the dataset
+    return input_function # returns a function object for use
+
+train_input_fn = make_input_fn(dftrain, y_train) # calling the input function that was returned above
+eval_input_fn = make_input_fn(dfeval, y_eval, num_epochs=1, shuffle=False)
+
+# linear_est = tf.estimator.LinearClassifier(feature_columns=feature_columns)
+linear_est = tf.estimator.LinearClassifier(feature_columns=feature_columns) # ! Estimator is depricated!
+
+linear_est.train(train_input_fn) # train
+result = linear_est.evaluate(eval_input_fn) # get model metrics/states by testing on testing data
+
+clear_output() # clears console output
+print(result['accuracy']) # the result variable is simply a dict of stats about out model
